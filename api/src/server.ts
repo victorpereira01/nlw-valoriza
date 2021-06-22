1802c87844324ab89a08287e1a1e6270c7d0407a
Victor Pereira <victor.cordeiro2@gmail.com>
2021-06-22 16:51:42 -0300

    Rotas de teste

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get("/test", (req, res) => {
+    return res.send("OLA NLW");
+});
+
+app.post("/test-post", (req, res) => {
+    return res.send("OLA POST NLW");
+});
+
 app.listen(3000, () => console.log("Server is running!"));
